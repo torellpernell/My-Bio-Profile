092cb51ce7a553ec7ddcf881e5ea923e40e3ceb5
Torell <tpernell@icstars.org>
2025-12-10 11:44:01 -0600

    new commit

---
+++ backend/src/lib/prisma.js
@@ -0,0 +1,17 @@
+import { PrismaMssql } from "@prisma/adapter-mssql";
+import { PrismaClient } from "@prisma/client";
+
+const config = {
+  server: "localhost",
+  port: 1433,
+  database: "DevLogs",
+  user: "tpernell@icstars.org",
+  password: "Dad022679!",
+  options: {
+    encrypt: true, // Use this if you're on Windows Azure
+    trustServerCertificate: true, // Use this if you're using self-signed certificates
+  },
+};
+
+const adapter = new PrismaMssql(config);
+const prisma = new PrismaClient({ adapter });
